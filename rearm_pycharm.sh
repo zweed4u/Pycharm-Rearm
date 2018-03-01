2940e18f8a0d57f0e35b287a0a6f527867656cb5
Zachary Weeden <zdw7287@rit.edu>
2018-02-28 20:46:10 -0500

    Backing up original files

--- rearm_pycharm.sh
+++ rearm_pycharm.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+cp ~/.PyCharm2017.3/config/eval/PyCharm173.evaluation.key ~/.PyCharm2017.3/config/eval/PyCharm173.evaluation.key.bak
 rm ~/.PyCharm2017.3/config/eval/PyCharm173.evaluation.key
+
+cp ~/.PyCharm2017.3/config/options/options.xml ~/.PyCharm2017.3/config/options/options.xml.bak
 sed -i '/evlsprt/d' ~/.PyCharm2017.3/config/options/options.xml
+
+cp ~/.java/.userPrefs/prefs.xml ~/.java/.userPrefs/prefs.xml.bak
 sed -i '/evlsprt/d' ~/.java/.userPrefs/prefs.xml
