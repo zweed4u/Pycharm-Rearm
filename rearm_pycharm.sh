1cf7568aa39abbab3915621dea6ddba86d4b6adb
Zachary Weeden <zdw7287@rit.edu>
2018-02-28 20:44:44 -0500

    Ridding of evlsprt string

--- rearm_pycharm.sh
+++ rearm_pycharm.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 rm ~/.PyCharm2017.3/config/eval/PyCharm173.evaluation.key
+sed -i '/evlsprt/d' ~/.PyCharm2017.3/config/options/options.xml
+sed -i '/evlsprt/d' ~/.java/.userPrefs/prefs.xml
